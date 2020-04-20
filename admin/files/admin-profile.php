c74c7066088d96ccebed9875b057d427d23a6207
Hamza Abdiqadir <hamanto96@gmail.com>
2020-04-20 21:55:40 +0300

    added functionality to allow admins to update their information

--- admin/files/admin-profile.php
+++ admin/files/admin-profile.php
@@ -17,7 +17,43 @@ header('Location:admin-login.php');
 unset($_SESSION['CREATED']);
 }
 
+//edit and delete admin profile 
 
+
+
+if(isset($_POST['update'])){
+  $session_username = $_SESSION['ausername'];
+  $username = $_POST['username'];
+  $firstname = $_POST['fname'];
+  $lastname = $_POST['lname'];
+  $email = $_POST['email'];
+  $password = md5(mysqli_real_escape_string($conn, $_POST['password'])); 
+  $address = $_POST['address'];
+  $phone = $_POST['phone'];
+
+
+  $query= "update ADMIN set firstname='$firstname', lastname='$lastname', email='$email',
+  phoneNo='$phone', c_address='$address',username='$username', password='$password' where username='$session_username' ";
+  echo $query;
+
+  $exec = mysqli_query($conn, $query);
+  if(!$exec){
+    
+    echo 'error while updating profile' . mysqli_error($conn);
+  }
+  else{
+    ?> 
+    <div class='alert alert-success' style="margin-top:70px; text-align:center;" ><?php echo 'successful update on user:  '.$session_username?>
+  
+    </div>
+    
+  
+  <?php
+ 
+  header("Refresh: 3; url='admin-login.php'");
+  unset($_SESSION['ausername']);
+  }
+}
 
 
 
@@ -29,7 +65,7 @@ unset($_SESSION['CREATED']);
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>
-    Argon Dashboard - Free Dashboard for Bootstrap 4 by Creative Tim
+   
   </title>
   <!-- Favicon -->
   <link href="../assets/img/brand/favicon.png" rel="icon" type="image/png">
@@ -136,70 +172,40 @@ unset($_SESSION['CREATED']);
         <!-- Navigation -->
         <ul class="navbar-nav">
           <li class="nav-item  active ">
-            <a class="nav-link " href="../index.html">
+            <a class="nav-link  active " href="./admin-index.php">
               <i class="ni ni-tv-2 text-primary"></i> Dashboard
             </a>
           </li>
+       
           <li class="nav-item">
-            <a class="nav-link " href="../icons.html">
-              <i class="ni ni-planet text-blue"></i> Icons
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="../inventory.php">
+            <a class="nav-link " href="./inventory.php">
               <i class="ni ni-pin-3 text-orange"></i> Inventory
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link  active " href="../files/admin-profile.php">
+            <a class="nav-link " href="./admin-profile.php">
               <i class="ni ni-single-02 text-yellow"></i> User profile
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link " href="../sales.php">
+            <a class="nav-link " href="./sales.php">
               <i class="ni ni-bullet-list-67 text-red"></i> Sales
             </a>
           </li>
           <!-- <li class="nav-item">
-            <a class="nav-link" href="../files/login.html">
+            <a class="nav-link" href="./files/admin-login.php">
               <i class="ni ni-key-25 text-info"></i> Login
             </a>
           </li> -->
           <li class="nav-item">
-            <a class="nav-link" href="../admin-register.php">
+            <a class="nav-link" href="./admin-register.php">
               <i class="ni ni-circle-08 text-pink"></i> Register
             </a>
           </li>
         </ul>
         <!-- Divider -->
         <hr class="my-3">
-        <!-- Heading -->
-        <!-- <h6 class="navbar-heading text-muted">Documentation</h6> -->
-        <!-- Navigation -->
-        <!-- <ul class="navbar-nav mb-md-3">
-          <li class="nav-item">
-            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">
-              <i class="ni ni-spaceship"></i> Getting started
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">
-              <i class="ni ni-palette"></i> Foundation
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html">
-              <i class="ni ni-ui-04"></i> Components
-            </a>
-          </li>
-        </ul>
-        <ul class="navbar-nav">
-          <li class="nav-item active active-pro">
-            <a class="nav-link" href="../files/upgrade.html">
-              <i class="ni ni-send text-dark"></i> Upgrade to PRO
-            </a>
-          </li>
-        </ul> -->
+    
       </div>
     </div>
   </nav>
@@ -265,16 +271,17 @@ unset($_SESSION['CREATED']);
     </nav>
     <!-- End Navbar -->
     <!-- Header -->
-    <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center" style="min-height: 600px; background-image: url(../assets/img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
+    <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center" style="min-height: 600px; background-image: url(../assets/img/theme/hand.jpg); background-size: cover; background-position: center top;">
       <!-- Mask -->
       <span class="mask bg-gradient-default opacity-8"></span>
       <!-- Header container -->
       <div class="container-fluid d-flex align-items-center">
         <div class="row">
           <div class="col-lg-7 col-md-10">
-            <h1 class="display-2 text-white">Hello Jesse</h1>
+            <h1 class="display-2 text-white">Hello <?php print_r($_SESSION['ausername']); ?></h1>
             <p class="text-white mt-0 mb-5">This is your profile page. You can see the progress you've made with your work and manage your projects or assigned tasks</p>
-            <button type="submit" class="btn btn-info">Edit Profile</button>
+            
+            
           </div>
         </div>
       </div>
@@ -288,7 +295,8 @@ unset($_SESSION['CREATED']);
               <div class="col-lg-3 order-lg-2">
                 <div class="card-profile-image">
                   <a href="#">
-                    <img src="../assets/img/theme/team-4-800x800.jpg" class="rounded-circle">
+                  <i class="ni ni-circle-08 text-blue"></i>
+                    <img src="../assets/img/theme/grey.jpg" class="rounded-circle">
                   </a>
                 </div>
               </div>
@@ -303,37 +311,15 @@ unset($_SESSION['CREATED']);
               <div class="row">
                 <div class="col">
                   <div class="card-profile-stats d-flex justify-content-center mt-md-5">
-                    <div>
-                      <span class="heading">22</span>
-                      <span class="description">Friends</span>
-                    </div>
-                    <div>
-                      <span class="heading">10</span>
-                      <span class="description">Photos</span>
-                    </div>
-                    <div>
-                      <span class="heading">89</span>
-                      <span class="description">Comments</span>
-                    </div>
+                  
                   </div>
                 </div>
               </div>
               <div class="text-center">
-                <h3>
-                  <?php print_r($_SESSION['ausername']); ?> Jones<span class="font-weight-light">, 27</span>
+                <h3>Admin Name: <br>
+                  <?php print_r($_SESSION['ausername']); ?> <span class="font-weight-light"></span>
                 </h3>
-                <div class="h5 font-weight-300">
-                  <i class="ni location_pin mr-2"></i>Bucharest, Romania
-                </div>
-                <div class="h5 mt-4">
-                  <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
-                </div>
-                <div>
-                  <i class="ni education_hat mr-2"></i>University of Computer Science
-                </div>
-                <hr class="my-4" />
-                <p>Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music.</p>
-                <a href="#">Show more</a>
+               
               </div>
             </div>
           </div>
@@ -351,20 +337,21 @@ unset($_SESSION['CREATED']);
               </div>
             </div>
             <div class="card-body">
-              <form>
+              <form method='post' action='admin-profile.php'>
                 <h6 class="heading-small text-muted mb-4">User information</h6>
                 <div class="pl-lg-4">
                   <div class="row">
                     <div class="col-lg-6">
                       <div class="form-group">
+
                         <label class="form-control-label" for="input-username">Username</label>
-                        <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Username" value="lucky.jesse">
+                        <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Username"  name='username'>
                       </div>
                     </div>
                     <div class="col-lg-6">
                       <div class="form-group">
                         <label class="form-control-label" for="input-email">Email address</label>
-                        <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="jesse@example.com">
+                        <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="xyz@example.com" name='email'>
                       </div>
                     </div>
                   </div>
@@ -372,59 +359,49 @@ unset($_SESSION['CREATED']);
                     <div class="col-lg-6">
                       <div class="form-group">
                         <label class="form-control-label" for="input-first-name">First name</label>
-                        <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="Lucky">
+                        <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" name='fname'>
                       </div>
                     </div>
                     <div class="col-lg-6">
                       <div class="form-group">
                         <label class="form-control-label" for="input-last-name">Last name</label>
-                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="Jesse">
+                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name"  name='lname'>
                       </div>
                     </div>
                   </div>
                 </div>
-                <hr class="my-4" />
-                <!-- Address -->
-                <h6 class="heading-small text-muted mb-4">Contact information</h6>
+                
                 <div class="pl-lg-4">
                   <div class="row">
                     <div class="col-md-12">
                       <div class="form-group">
                         <label class="form-control-label" for="input-address">Address</label>
-                        <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
+                        <input  class="form-control form-control-alternative" name='address' placeholder="Home Address"  type="text">
                       </div>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-lg-4">
+               
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
                       <div class="form-group">
-                        <label class="form-control-label" for="input-city">City</label>
-                        <input type="text" id="input-city" class="form-control form-control-alternative" placeholder="City" value="New York">
+                        <label class="form-control-label" for="input-first-name">Phone Number</label>
+                        <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="Phone number"  name='phone'>
                       </div>
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col-lg-6">
                       <div class="form-group">
-                        <label class="form-control-label" for="input-country">Country</label>
-                        <input type="text" id="input-country" class="form-control form-control-alternative" placeholder="Country" value="United States">
-                      </div>
-                    </div>
-                    <div class="col-lg-4">
-                      <div class="form-group">
-                        <label class="form-control-label" for="input-country">Postal code</label>
-                        <input type="number" id="input-postal-code" class="form-control form-control-alternative" placeholder="Postal code">
+                        <label class="form-control-label" for="input-last-name">password</label>
+                        <input type="password" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name"  name='password'>
                       </div>
                     </div>
                   </div>
                 </div>
+                <form action="admin-profile.php" method="post">
+            <button type="submit" class="btn btn-info" name='update'>Edit Profile</button>
+            </form>
                 <hr class="my-4" />
-                <!-- Description -->
-                <h6 class="heading-small text-muted mb-4">About me</h6>
-                <div class="pl-lg-4">
-                  <div class="form-group">
-                    <label>About Me</label>
-                    <textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
-                  </div>
-                </div>
+
               </form>
             </div>
           </div>
